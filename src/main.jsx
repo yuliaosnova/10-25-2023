00d5a0485024b2641db7da261814ed25e520b0fb
Yulia Osnova <yulia.osnova@gmail.com>
2023-10-25 21:39:43 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./components/App.jsx";
 import { Provider } from "./components/Context/Context.jsx";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
